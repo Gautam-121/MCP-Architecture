af5db1989ace769ea0012d30231db35c8dfc6c66
Gautam-121 <gautamdhakate1234@gmail.com>
2025-03-29 16:13:15 +0530

    add vercel configration file

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config({path:"./.env"});
-
-console.log("MongoUri" , process.env.MONGO_URI);
+dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
